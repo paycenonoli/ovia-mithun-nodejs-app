eb1aa17d2b26f0dae1686b26a4382230eec88be9
paycenonoli <119795130+paycenonoli@users.noreply.github.com>
2025-06-30 09:09:27 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Applicasqu_5990737adba596165bfee17b0c38ea5b1616b6e7
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_5990737adba596165bfee17b0c38ea5b1616b6e7',
+	    'sonar.login': 'squ_255221a7a6c86a3f4239b0dd0cc65031886c5481',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
